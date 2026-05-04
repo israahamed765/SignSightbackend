17a51b9c11b098f3315630fd9f8dadc778f7f0ed
israahamed765 <tsraathmd@gmail.com>
2026-05-04 11:31:24 +0300

    Prepare backend for deployment

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,36 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // 1. إعدادات الرفع (لحل مشكلة الـ EPERM والـ 500 Error)
+  upload: {
+    config: {
+      // path: config/plugins.js
+
+      breakpoints: {
+        large: 1000,
+        medium: 750,
+        small: 500,
+      },
+      // تعطيل توليد الأحجام الإضافية يمنع ويندوز من منع حذف الملفات المؤقتة
+      responsiveDimensions: false,
+    },
+  },
+
+  // 2. إعدادات الإيميل (التي كانت لديكِ سابقاً)
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: env("GMAIL_USER"),
+          pass: env("GMAIL_PASS"),
+        },
+      },
+      settings: {
+        defaultFrom: "SignSight Team <120211325@std.up.edu.ps>",
+        defaultReplyTo: "120211325@std.up.edu.ps",
+      },
+    },
+  },
+});
